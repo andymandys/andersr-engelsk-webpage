7b61e5db3a6db6a7661796748e6b73581910ad7b
AndersR <ander.kvinge@gmail.com>
2026-01-05 21:51:34 +0100

    Her har jeg lagt til mobil support(Tror jeg) Og Lagt til local storage til darkmode sånn at det holder seg dersom man refresher pagen.

--- darkmode.js
+++ darkmode.js
@@ -1,20 +1,41 @@
 const knapp = document.getElementById("toggleDarkMode")
-const body = document.querySelector ("body")
+const body = document.querySelector("body")
+
+console.log(localStorage.getItem("darkmode-active"));
+
+// Før siden loader, sjekk hvis siden skal ha dark mode
+if (localStorage.getItem("darkmode-active") === "true") {
+    // Set siden til darkmode
+    setDarkMode();
+}
+
+function setLightMode() {
+    body.classList.remove("darkmode")
+    knapp.firstChild.classList.add("fa-moon")
+    knapp.firstChild.classList.remove("fa-lightbulb")
+
+    // Lagre i siden at darkmode ikke er aktiv
+    localStorage.setItem("darkmode-active", "false");
+}
+
+function setDarkMode() {
+    body.classList.add("darkmode")
+    knapp.firstChild.classList.remove("fa-moon")
+    knapp.firstChild.classList.add("fa-lightbulb")
+
+    // Lagre i siden at darkmode er aktiv
+    localStorage.setItem("darkmode-active", "true");
+}
 
 function toggleDarkMode() {
     if (body.classList.contains("darkmode")) {
-        body.classList.remove("darkmode")
-        knapp.firstChild.classList.add("fa-moon")
-        knapp.firstChild.classList.remove("fa-lightbulb")
-        
-
+        setLightMode();
     }
     else {
-        body.classList.add("darkmode")
-        knapp.firstChild.classList.remove("fa-moon")
-        knapp.firstChild.classList.add("fa-lightbulb")
-
+        setDarkMode();
     }
 }
+
+// Legg til toggleDarkMode funksjonen til toggleDarkmode knappen. 
 knapp.addEventListener("click", toggleDarkMode)
 
